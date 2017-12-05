87e202184c134b0335c26df4c3a9d3ebc007631a
slie07 <liestevano@gmail.com>
2017-12-05 13:05:13 -0700

    Add files via upload
    reduce.js

--- myReduce.js
+++ myReduce.js
@@ -3,7 +3,26 @@
 
 function myReduce(arr, callback) {
 
-//  CODE INSIDE HERE   //
+// first pass is special because it uses initialValue rather than the result
+  //    from the last run.
+  var i=0;  // setting this here so we can increment it +1 when no initialValue
+  var previousValue;
+
+  // if an initialValue is not passed in we'll use the first two elements on the
+  //   first pass
+  if(typeof(initialValue) == "undefined") {
+    i++;
+    previousValue = arr[0];
+  } else {
+    previousValue = initialValue;
+  }
+
+  // loop of the array calling the callback each time
+  for (; i<arr.length; i++) {
+    previousValue = callback(previousValue, arr[i], i, arr);
+  }
+  return previousValue;
+
 
 }
 
